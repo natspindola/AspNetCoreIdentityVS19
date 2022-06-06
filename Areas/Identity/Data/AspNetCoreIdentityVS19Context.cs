e8b6270af37c117368066efeb0d03b9d8af6efc4
Perk <natspindola@hotmail.com>
2022-06-06 17:36:41 -0300

    Add project files.

---
+++ Areas/Identity/Data/AspNetCoreIdentityVS19Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreIdentityVS19.Areas.Identity.Data
+{
+    public class AspNetCoreIdentityVS19Context : IdentityDbContext<IdentityUser>
+    {
+        public AspNetCoreIdentityVS19Context(DbContextOptions<AspNetCoreIdentityVS19Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
