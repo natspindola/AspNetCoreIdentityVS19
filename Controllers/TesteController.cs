dc0ce09108d02c8bd09593b5b6222ab27b3aae7c
Perk <natspindola@hotmail.com>
2022-06-09 17:53:58 -0300

    iniciando o LOG

---
+++ Controllers/TesteController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreIdentityVS19.Controllers
+{
+    public class TesteController : Controller
+    {
+        private readonly ILogger<TesteController> _logger;
+
+        public TesteController(ILogger<TesteController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogError(message: "Esse erro aconteceu!");
+
+            return View();
+        }
+    }
+}
