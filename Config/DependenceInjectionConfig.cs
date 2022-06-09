34e62f8409d50d8e77b2587d29ec25f62d1cc349
Perk <natspindola@hotmail.com>
2022-06-09 19:14:15 -0300

    configurando KissLog

--- Config/DependenceInjectionConfig.cs
+++ Config/DependenceInjectionConfig.cs
@@ -1,12 +1,5 @@
-﻿using AspNetCoreIdentityVS19.Areas.Identity.Data;
-using AspNetCoreIdentityVS19.Extensions;
+﻿using AspNetCoreIdentityVS19.Extensions;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNetCoreIdentityVS19.Config
@@ -17,25 +10,6 @@ namespace AspNetCoreIdentityVS19.Config
         {
             services.AddSingleton<IAuthorizationHandler, PermissaoNecessariaHendler>();
             
-            return services;
-        }
-
-        public static IServiceCollection AddIdentityConfig(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-            services.AddDbContext<AspNetCoreIdentityVS19Context>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("AspNetCoreIdentityVS19ContextConnection")));
-
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddRoles<IdentityRole>()
-                .AddDefaultUI(UIFramework.Bootstrap4)
-                .AddEntityFrameworkStores<AspNetCoreIdentityVS19Context>();
-
             return services;
         }
     }
--- Config/IdentityConfig.cs
+++ Config/IdentityConfig.cs
@@ -1,5 +1,12 @@
-﻿using AspNetCoreIdentityVS19.Extensions;
+﻿using AspNetCoreIdentityVS19.Areas.Identity.Data;
+using AspNetCoreIdentityVS19.Extensions;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNetCoreIdentityVS19.Config
@@ -15,6 +22,26 @@ namespace AspNetCoreIdentityVS19.Config
                 options.AddPolicy(name: "PodeLer", configurePolicy: policy => policy.Requirements.Add(new PermissaoNecessaria(permissao: "PodeLer")));
                 options.AddPolicy(name: "PodeEscrever", configurePolicy: policy => policy.Requirements.Add(new PermissaoNecessaria(permissao: "PodeEscrever")));
             });
+
+            return services;
+        }
+
+
+        public static IServiceCollection AddIdentityConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDbContext<AspNetCoreIdentityVS19Context>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("AspNetCoreIdentityVS19ContextConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddDefaultUI(UIFramework.Bootstrap4)
+                .AddEntityFrameworkStores<AspNetCoreIdentityVS19Context>();
 
             return services;
         }
