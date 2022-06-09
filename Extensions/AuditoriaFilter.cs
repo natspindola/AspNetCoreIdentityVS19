1f6d86afd01a59628e5efcf98a3b3c7cc4d73998
Perk <natspindola@hotmail.com>
2022-06-09 20:01:21 -0300

    implementando filtro de ação

---
+++ Extensions/AuditoriaFilter.cs
@@ -0,0 +1,33 @@
+﻿using KissLog;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreIdentityVS19.Extensions
+{
+    public class AuditoriaFilter : IActionFilter
+    {
+        private readonly ILogger _logger;
+
+        public AuditoriaFilter(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context) {}
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if(context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                var message = context.HttpContext.User.Identity.Name + "Acessou: " +
+                    context.HttpContext.Request.GetDisplayUrl();
+
+                _logger.Info(message);
+            }
+        }
+    }
+}
